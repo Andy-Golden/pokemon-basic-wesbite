8111d62129b2b5e7d4dc497df68d3b9c83062ef6
Andy Nguyen <andy.nguyen.goldenowl@gmail.com>
2023-10-12 13:43:40 +0700

    fix: does not change name in modal when submit

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ const colorTypes = {
 };
 
 //curry function
-const handleOpenModal = (id, name) => (e) => {
+const handleOpenModal = (id) => () => {
+  const detailPokes = JSON.parse(localStorage.getItem("detailPokes"));
+  const poke = detailPokes.find((detailPoke) => detailPoke.id == id);
+
   const modal = document.getElementById("bg-modal");
   modal.classList.add("bg-modal--show");
   const input = modal.getElementsByTagName("input")[0];
-  input.value = name;
-
+  input.value = poke.name;
   localStorage.setItem("pokeId", id);
 };
 
@@ -23,14 +25,32 @@ const formModal = document.getElementById("form-modal");
 
 formModal.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const pokeId = localStorage.getItem("pokeId");
+  const detailPokes = JSON.parse(localStorage.getItem("detailPokes"));
+
   const newPokeName = document.getElementById("pokemon-name").value;
   document.getElementById(`pokemon-name${pokeId}`).innerHTML = newPokeName;
+
+  detailPokes.forEach((detailPoke) => {
+    if (detailPoke.id == pokeId) {
+      detailPoke.name = newPokeName;
+    }
+  });
+
+  console.log(detailPokes[0].name);
+
+  localStorage.setItem("detailPokes", JSON.stringify(detailPokes));
+
+  const modalClassList = document.getElementById("bg-modal").classList;
+  modalClassList.remove("bg-modal--show");
 });
 
 window.onload = async () => {
+  console.log("load");
   const data = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=25");
   const listPoke = await data.json();
+
   let requests = listPoke?.results.map((res) => fetch(res.url));
 
   Promise.all(requests)
@@ -55,14 +75,14 @@ window.onload = async () => {
         <div id="elelements${detailPoke.id}" class="card-pokemon__elements">
         </div>
       </div>
-    </div>`;
+      </div>`;
 
         const template = document.createElement("template");
         template.innerHTML = htmlString;
         document.getElementById("list-pokemon").appendChild(template.content);
 
         document.getElementById(`card-pokemon-btn${detailPoke.id}`).onclick =
-          handleOpenModal(detailPoke.id, detailPoke.name);
+          handleOpenModal(detailPoke.id);
 
         for (let i = 0; i < detailPoke.types.length; i++) {
           const elelementsTemplate = document.createElement("template");
@@ -90,28 +110,29 @@ async function handleSearch(e) {
       await fetch(`https://pokeapi.co/api/v2/pokemon/${idPoke}`)
     ).json();
 
-    const htmlString = `<div class="card-pokemon">
-            <img
-              class="card-pokemon__image"
-              src="${detailPoke.sprites.front_default}"
-              alt="poke vatar"
-            />
-            <button id="card-pokemon-btn${detailPoke.id}" class="card-pokemon__button">
-              Edit       
-            </button>
-            <div class="card-pokemon__content">
-              <div class="card-pokemon__info">
-                <p class="card-pokemon__id">n 00${detailPoke.id}</p>
-                <p class="card-pokemon__name">${detailPoke.name}</p>
-              </div>
-              <div class="card-pokemon__elements">
-                <p class="card-pokemon__element">Grass</p>
-                <p class="card-pokemon__element">Grass</p>
-              </div>
-            </div>
-          </div>`;
+    const htmlString = `<div draggable="true" class="card-pokemon">
+    <img
+      class="card-pokemon__image"
+      src="${detailPoke.sprites.front_default}" 
+      alt="poke vatar"
+    />
+    <button id="card-pokemon-btn${detailPoke.id}" class="card-pokemon__button">
+      Edit       
+    </button>
+    <div class="card-pokemon__content">
+      <div class="card-pokemon__info">
+        <p class="card-pokemon__id">n 00${detailPoke.id}</p>
+        <p id="pokemon-name${detailPoke.id}" class="card-pokemon__name">${detailPoke.name}</p>
+      </div>
+      <div id="elelements${detailPoke.id}" class="card-pokemon__elements">
+      </div>
+    </div>
+    </div>`;
 
     document.getElementById("list-pokemon").innerHTML = htmlString;
+
+    document.getElementById(`card-pokemon-btn${detailPoke.id}`).onclick =
+      handleOpenModal(detailPoke.id);
   } catch (err) {
     console.log("🚀 ~ file: script.js:45 ~ btnSearchClicked ~ err:", err);
   }
@@ -157,16 +178,19 @@ function handleFilter() {
       <div class="card-pokemon__content">
         <div class="card-pokemon__info">
           <p class="card-pokemon__id">n 00${detailPoke.id}</p>
-          <p class="card-pokemon__name">${detailPoke.name}</p>
+          <p id="pokemon-name${detailPoke.id}" class="card-pokemon__name">${detailPoke.name}</p>
         </div>
         <div id="elelements${detailPoke.id}" class="card-pokemon__elements">
         </div>
       </div>
-    </div>`;
+      </div>`;
 
       const template = document.createElement("template");
       template.innerHTML = htmlString;
       document.getElementById("list-pokemon").appendChild(template.content);
+
+      document.getElementById(`card-pokemon-btn${detailPoke.id}`).onclick =
+        handleOpenModal(detailPoke.id);
 
       for (let i = 0; i < detailPoke.types.length; i++) {
         const elelementsTemplate = document.createElement("template");
